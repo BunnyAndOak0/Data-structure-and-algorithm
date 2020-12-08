e28c45795d119e6babf932f40e4a79ff3790d5f3
BunnyAndOak0 <1836983989@qq.com>
2020-12-08 08:15:28 +0800

    12.8

---
+++ src/edu/kmust/recursion/Queue8.java
@@ -0,0 +1,72 @@
+package edu.kmust.recursion;
+
+/**
+ * @author BunnyAndOak0
+ * TODO 八皇后算法问题
+ * Dec 7, 2020
+ */
+public class Queue8 {
+	//定义一个max表示共有多少个皇后
+	int max = 8;
+	//定义一个数组array，保存皇后放置的位置结果，比如arr = {0, 4, 7, 5, 2, 6, 1, 3}
+	int[] array = new int[max];
+	static int count = 0;
+	public static void main(String[] args) {
+		// 测试，8皇后是否正确
+	 	Queue8 queue8 = new Queue8();
+	 	queue8.check(0);	
+	 	System.out.printf("一共有%d种解法", count);
+	}
+	
+	//放置第n个皇后
+	//check是每一次递归时，进入到check中都有for(int i = 0; i < max; i++)，因此会有回溯
+	private void check(int n) {
+		if (n == max) {		//n = 8,其实8个皇后就已经放好了
+			print();
+			return;
+		}
+		
+		//依次放入皇后，并判断是否冲突
+		for (int i = 0; i < max; i++) {
+			//先把当前皇后n，放到该行的第一列
+			array[n] = i;
+			//判断当放置第n个皇后到i列时，是否冲突
+			if (judge(n)) {		//不冲突
+				//接着放 n+1个皇后，即开始递归
+				check(n + 1);
+			}
+			//如果冲突，就继续执行 array[n] = i;    即将第n个皇后，放置在本行后移的一个位置
+			
+		}
+	}
+	
+	
+	//当我们放置第n个皇后时，就检测该皇后是否和前面已经摆放的皇后冲突
+	/**
+	 * @param n 表示第n个皇后
+	 * @return
+	 */
+	private boolean judge(int n) {
+		for (int i = 0; i < n; i++) {
+			/**
+			 * 说明：
+			 * 1.array[i] == array[n]表示第n个皇后是否和n-1个皇后在同一列
+			 * 2.Math.abs(n - i) == Math.abs(array[n] - array[i]) 表示判断第n个皇后是否和第i个皇后是否在同一斜线
+			 * 3.没有必要判断是否在同一行，n每次都在递增
+			 */
+			if (array[i] == array[n] || Math.abs(n - i) == Math.abs(array[n] - array[i])) {    
+				return false;
+			}
+		}
+		return true;
+	}
+	
+	//写一个方法，可以将皇后摆放的的位置输出
+	private void print() {
+		count ++;
+		for (int i = 0; i < array.length; i++) {
+			System.out.print(array[i] + " ");
+		}
+		System.out.println();
+	}
+}
